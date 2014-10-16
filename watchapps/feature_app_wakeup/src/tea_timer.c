0466320ef20439841fc0ce8749f6dfd26331eb51
Katharine Berry <katharine@getpebble.com>
2014-10-16 12:23:20 -0700

    Releasing Examples for version 2.7

---
+++ watchapps/feature_app_wakeup/src/tea_timer.c
@@ -0,0 +1,285 @@
+#include <pebble.h>
+
+// Tea selection window
+static Window *s_menu_window;
+static MenuLayer *s_menu_layer;
+static TextLayer *s_error_text_layer;
+
+// Tea countdown window with cancel button
+static Window *s_countdown_window;
+static TextLayer *s_tea_text_layer;
+static char s_tea_text[32];
+static const int s_tea_text_gap = 14;
+static TextLayer *s_countdown_text_layer;
+static char s_countdown_text[32];
+static TextLayer *s_cancel_text_layer;
+
+// Tea is done window
+static Window *s_wakeup_window;
+static BitmapLayer *s_bitmap_layer;
+static GBitmap *s_gbitmap;
+
+static WakeupId s_wakeup_id = -1;
+static time_t s_wakeup_timestamp = 0;
+
+
+typedef struct {
+  char tea_name[16];  // Name of this tea
+  int tea_min;        // Minutes to steep this tea
+} TeaInfo;
+
+// Array of different teas for tea timer
+// {<Tea Name>, <Brew time in minutes>}
+TeaInfo tea_array[] = {
+  {"Green Tea", 1},
+  {"Black Tea", 2},
+  {"Oolong Tea", 3},
+  {"Darjeeling", 4},
+  {"Herbal Tea", 5},
+  {"Mate Tea", 6},
+  {"Chai Tea", 10}
+};
+
+enum {
+  PERSIST_WAKEUP // Persistent storage key for wakeup_id
+};
+
+static void menu_layer_select_callback(
+    struct MenuLayer *s_menu_layer, MenuIndex *cell_index, void *callback_context) {
+  // If we were displaying s_error_text_layer, remove it and return
+  if (!layer_get_hidden(text_layer_get_layer(s_error_text_layer))) {
+    layer_set_hidden(text_layer_get_layer(s_error_text_layer), true);
+    return;
+  }
+
+  // Wakeup time is a timestamp in the future
+  // so time(NULL) + delay_time_in_seconds = wakeup_time
+  time_t wakeup_time = time(NULL) + tea_array[cell_index->row].tea_min * 60;
+
+  // Use the tea_array index as the wakeup reason, so on wakeup trigger
+  // we know which tea is brewed
+  s_wakeup_id = wakeup_schedule(wakeup_time, cell_index->row, true);
+
+  // If we couldn't schedule the wakeup event, display error_text overlay
+  if (s_wakeup_id <= 0) {
+    layer_set_hidden(text_layer_get_layer(s_error_text_layer), false);
+    return;
+  }
+
+  // Store the handle so we can cancel if necessary, or look it up next launch
+  persist_write_int(PERSIST_WAKEUP, s_wakeup_id);
+
+  // Switch to countdown window
+  window_stack_push(s_countdown_window, false);
+}
+
+static uint16_t menu_layer_get_sections_count_callback(
+  struct MenuLayer *menulayer, uint16_t section_index, void *callback_context) {
+  int count = sizeof(tea_array) / sizeof(TeaInfo);
+  return count;
+}
+
+static void draw_row_handler(GContext *ctx, const Layer *cell_layer, MenuIndex *cell_index, void *callback_context) {
+  char* tea_name = tea_array[cell_index->row].tea_name;
+  int tea_min = tea_array[cell_index->row].tea_min;
+  int text_gap_size = s_tea_text_gap - strlen(tea_name);
+
+  // Using simple space padding between tea_name and s_tea_text for appearance of edge-alignment
+  snprintf(s_tea_text, sizeof(s_tea_text), "%s%*s%d min", tea_name, text_gap_size, "", tea_min);
+
+  menu_cell_basic_draw(ctx, cell_layer, s_tea_text, NULL, NULL);
+}
+
+static void menu_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  s_menu_layer = menu_layer_create(bounds);
+  menu_layer_set_callbacks(s_menu_layer, NULL, 
+    (MenuLayerCallbacks){
+    .get_num_rows = menu_layer_get_sections_count_callback,
+    .draw_row = draw_row_handler,
+    .select_click = menu_layer_select_callback
+    }); 
+  menu_layer_set_click_config_onto_window(s_menu_layer,	window);
+  layer_add_child(window_layer, menu_layer_get_layer(s_menu_layer));
+
+  s_error_text_layer = text_layer_create((GRect) { .origin = {0, 44}, .size = {bounds.size.w, 60}});
+  text_layer_set_text(s_error_text_layer, "Cannot\nschedule");
+  text_layer_set_text_alignment(s_error_text_layer, GTextAlignmentCenter);
+  text_layer_set_font(s_error_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+  text_layer_set_text_color(s_error_text_layer, GColorWhite);
+  text_layer_set_background_color(s_error_text_layer, GColorBlack);
+  layer_set_hidden(text_layer_get_layer(s_error_text_layer), true);
+  layer_add_child(window_layer, text_layer_get_layer(s_error_text_layer));
+}
+
+static void menu_window_unload(Window *window) {
+  menu_layer_destroy(s_menu_layer);
+  text_layer_destroy(s_error_text_layer);
+}
+
+static void timer_handler(void *data) {
+  if (s_wakeup_timestamp == 0) {
+    // get the wakeup timestamp for showing a countdown
+    wakeup_query(s_wakeup_id, &s_wakeup_timestamp);
+  }
+  int countdown = s_wakeup_timestamp - time(NULL);
+  snprintf(s_countdown_text, sizeof(s_countdown_text), "%d seconds", countdown);
+  layer_mark_dirty(text_layer_get_layer(s_countdown_text_layer));
+  app_timer_register(1000, timer_handler, data);
+}
+
+static void countdown_back_handler(ClickRecognizerRef recognizer, void *context) {
+  window_stack_pop_all(true); // Exit app while waiting for tea to brew
+}
+
+// Cancel the current wakeup event on the countdown screen
+static void countdown_cancel_handler(ClickRecognizerRef recognizer, void *context) {
+  wakeup_cancel(s_wakeup_id);
+  s_wakeup_id = -1;
+  persist_delete(PERSIST_WAKEUP);
+  window_stack_pop(true); // Go back to tea selection window
+}
+
+static void countdown_click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_BACK, countdown_back_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, countdown_cancel_handler);
+}
+
+static void countdown_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  window_set_click_config_provider(window, countdown_click_config_provider);
+
+  s_tea_text_layer = text_layer_create((GRect) { .origin = {0, 32}, .size = {bounds.size.w, 20}});
+  text_layer_set_text(s_tea_text_layer, "Steeping time left");
+  text_layer_set_text_alignment(s_tea_text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_tea_text_layer));
+
+  s_countdown_text_layer = text_layer_create((GRect) {.origin = {0, 72}, .size = {bounds.size.w, 20}});
+  text_layer_set_text(s_countdown_text_layer, s_countdown_text);
+  text_layer_set_text_alignment(s_countdown_text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_countdown_text_layer));
+
+  // Place a cancel "X" next to the bottom button to cancel wakeup timer
+  s_cancel_text_layer = text_layer_create((GRect) { .origin = {124, 116}, .size = {24, 28}});
+  text_layer_set_text(s_cancel_text_layer, "X");
+  text_layer_set_font(s_cancel_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+  text_layer_set_text_alignment(s_cancel_text_layer, GTextAlignmentLeft);
+  layer_add_child(window_layer, text_layer_get_layer(s_cancel_text_layer));
+
+  s_wakeup_timestamp = 0;
+  app_timer_register(0, timer_handler, NULL);
+}
+
+static void countdown_window_unload(Window *window) {
+  text_layer_destroy(s_countdown_text_layer);
+  text_layer_destroy(s_cancel_text_layer);
+  text_layer_destroy(s_tea_text_layer);
+}
+
+static void wakeup_click_handler(ClickRecognizerRef recognizer, void *context) {
+  // Exit app after tea is done
+  window_stack_pop_all(true);
+}
+
+static void wakeup_click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, wakeup_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, wakeup_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, wakeup_click_handler);
+  window_single_click_subscribe(BUTTON_ID_BACK, wakeup_click_handler);
+}
+
+static void wakeup_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  window_set_click_config_provider(window, wakeup_click_config_provider);
+
+  // Bitmap layer for wakeup "tea is ready" image
+  s_bitmap_layer = bitmap_layer_create(bounds);
+  s_gbitmap = gbitmap_create_with_resource(RESOURCE_ID_TEA_SIGN);
+  bitmap_layer_set_bitmap(s_bitmap_layer, s_gbitmap);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_bitmap_layer));
+}
+
+static void wakeup_window_unload(Window *window) {
+  gbitmap_destroy(s_gbitmap);
+  bitmap_layer_destroy(s_bitmap_layer);
+}
+
+static void wakeup_handler(WakeupId id, int32_t reason) {
+  //Delete persistent storage value
+  persist_delete(PERSIST_WAKEUP);
+
+  window_stack_push(s_wakeup_window, false);
+
+  vibes_double_pulse();
+}
+
+static void init(void) {
+  bool wakeup_scheduled = false;
+
+  // Check if we have already scheduled a wakeup event
+  // so we can transition to the countdown window
+  if (persist_exists(PERSIST_WAKEUP)) {
+    s_wakeup_id = persist_read_int(PERSIST_WAKEUP);
+    // query if event is still valid, otherwise delete
+    if (wakeup_query(s_wakeup_id, NULL)) {
+      wakeup_scheduled = true;
+    } else {
+      persist_delete(PERSIST_WAKEUP);
+      s_wakeup_id = -1;
+    }
+  }
+
+  s_menu_window = window_create();
+  window_set_window_handlers(s_menu_window, (WindowHandlers){
+    .load = menu_window_load,
+    .unload = menu_window_unload,
+  });
+
+  s_countdown_window = window_create();
+  window_set_window_handlers(s_countdown_window, (WindowHandlers){
+    .load = countdown_window_load,
+    .unload = countdown_window_unload,
+  });
+
+  s_wakeup_window = window_create();
+  window_set_window_handlers(s_wakeup_window, (WindowHandlers){
+    .load = wakeup_window_load,
+    .unload = wakeup_window_unload,
+  });
+
+  // Check to see if we were launched by a wakeup event
+  if (launch_reason() == APP_LAUNCH_WAKEUP) {
+    // If woken by wakeup event, get the event display "tea is ready"
+    WakeupId id = 0;
+    int32_t reason = 0;
+    if (wakeup_get_launch_event(&id, &reason)) {
+      wakeup_handler(id, reason);
+    }
+  } else if (wakeup_scheduled) {
+    window_stack_push(s_countdown_window, false);
+  } else {
+    window_stack_push(s_menu_window, false);
+  }
+
+  // subscribe to wakeup service to get wakeup events while app is running
+  wakeup_service_subscribe(wakeup_handler);
+}
+
+static void deinit(void) {
+  window_destroy(s_menu_window);
+}
+
+int main(void) {
+  init();
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", s_menu_window);
+
+  app_event_loop();
+  deinit();
+}
---
+++ watchapps/feature_frame_buffer/src/feature_frame_buffer.c
@@ -0,0 +1,253 @@
+#include <pebble.h>
+
+static Window *s_window;
+static GSize s_window_size;
+
+/*
+ * Gray values for the animated gradient.
+ */
+int s_gray_top;
+int s_gray_bottom;
+
+/*
+ * Helper value for draw_method_feedback().
+ */
+int s_time_feedback_y;
+
+/*
+ * Bayer matrix for ordered dithering.
+ *
+ * Have a look at http://en.wikipedia.org/wiki/Ordered_dithering
+ */
+static const uint8_t ditherMatrix[8][8] = {
+  {  0, 128,  32, 160,   8, 136,  40, 168},
+  {192,  64, 224,  96, 200,  72, 232, 104},
+  { 48, 176,  16, 144,  56, 184,  24, 152},
+  {240, 112, 208,  80, 248, 120, 216,  88},
+  { 12, 140,  44, 172,   4, 132,  36, 164},
+  {204,  76, 236, 108, 196,  68, 228, 100},
+  { 60, 188,  28, 156,  52, 180,  20, 148},
+  {252, 124, 220,  92, 244, 116, 212,  84}
+};
+
+/*
+ * Converts a gray value to CGColor by using ordered dithering.
+ *
+ * As ordered dithering uses a stable pattern relative to the output image
+ * you need to specific the target coordinate of the resulting pixel.
+ *
+ */
+static GColor color_for_gray(int16_t x, int16_t y, uint8_t gray) {
+  // dithering performs a quantizing step to map many colors to a reduced set of colors.
+  // it does so by comparing the actual color (here: gray) against a threshold to ultimately
+  // find the closet matching color of the given palette (here: black or white)
+  // please read http://en.wikipedia.org/wiki/Ordered_dithering for details
+
+  // here's a simple implementation that does this with an ordered dithering matrix and
+  // only two possible output colors
+
+  // find the threshold value for a given pixel coordinate by "looping through" the dither matrix
+  const uint8_t threshold_value = ditherMatrix[y % 8][x % 8];
+
+  // with only two colors (black and white) finding the closest matching color is trivial
+  return gray > threshold_value ? GColorWhite : GColorBlack;
+}
+
+/*
+ * Calculates GColor values for 8 adjacent pixels at once and returns them as a single byte.
+ */
+static uint8_t dither_pattern_8(int16_t y, uint8_t gray) {
+  return color_for_gray(0, y, gray) << 7 |
+         color_for_gray(1, y, gray) << 6 |
+         color_for_gray(2, y, gray) << 5 |
+         color_for_gray(3, y, gray) << 4 |
+         color_for_gray(4, y, gray) << 3 |
+         color_for_gray(5, y, gray) << 2 |
+         color_for_gray(6, y, gray) << 1 |
+         color_for_gray(7, y, gray) << 0;
+}
+
+/*
+ * Draws textual feedback about the currently used update procedure.
+ *
+ * Y-coordinate is determined by s_time_feedback_y which is updated by update_animation().
+ *
+ */
+static void draw_method_feedback(GContext *ctx, char *method_text) {
+  GFont font = fonts_get_system_font(FONT_KEY_GOTHIC_18);
+  graphics_context_set_fill_color(ctx, GColorBlack);
+
+  struct GRect text_bounds = {.size = s_window_size};
+  text_bounds.size.h = (int16_t)(graphics_text_layout_get_content_size(method_text, font, text_bounds,
+      GTextOverflowModeTrailingEllipsis, GTextAlignmentCenter).h + 5); // space for descender
+  text_bounds.origin.y = (int16_t)(s_time_feedback_y % (s_window_size.h - text_bounds.size.h));
+
+  graphics_fill_rect(ctx, text_bounds, 0, GCornerNone);
+  graphics_draw_text(ctx, method_text, font, text_bounds,
+      GTextOverflowModeTrailingEllipsis, GTextAlignmentCenter, NULL);
+}
+
+/*
+ * Returns interpolated gray value between s_gray_top and s_gray_bottom for a given row.
+ */
+uint8_t gray_for_row(int16_t y) {
+  const int16_t h = s_window_size.h;
+
+  // linear interpolation without floats, equivalent to
+  //   float f = (float)y / (float)h;
+  //   return = (uint8_t)(s_gray_top * (1-f) + f * s_gray_bottom);
+  return (uint8_t)((s_gray_top * (h - y) + y * s_gray_bottom) / h);
+}
+
+/*
+ * Draws linear gradient using graphics_draw_pixel() + draws info about used method.
+ */
+static void update_proc_draw_pixel(Layer *layer, GContext *ctx) {
+  // determines gray value per row,
+  // calculates dithered pixel color per pixel, and
+  // uses graphics_draw_pixel for each pixel to draw the actual color
+
+  for (int16_t y = 0; y < s_window_size.h; y++) {
+    const uint8_t row_gray = gray_for_row(y);
+    for (int16_t x = 0; x < s_window_size.w; x++) {
+      graphics_context_set_stroke_color(ctx, color_for_gray(x, y, row_gray));
+      graphics_draw_pixel(ctx, GPoint(x, y));
+    }
+  }
+  draw_method_feedback(ctx, "draw_pixel");
+}
+
+/*
+ * Draws linear gradient using direct frame buffer access.
+ *
+ * This function isn't being used directly by any layer. Instead, update_proc_frame_buffer_1()
+ * and update_proc_frame_buffer_20() will call it before they draw additional information.
+ */
+static void update_proc_frame_buffer(Layer *layer, GContext *ctx) {
+  // obtains direct access to the frame buffer,
+  // determines gray_ values per row
+  // calculates dithering pattern for the row, and
+  // sets all pixels of the row at once
+
+  // obtain frame buffer (must be released, again using graphics_release_frame_buffer!)
+  GBitmap *fb = graphics_capture_frame_buffer(ctx);
+
+  // during the loop, row always points to the byte of the first 8 pixels of the current row
+  uint8_t *row = (uint8_t *)fb->addr;
+  row += fb->bounds.origin.y * fb->row_size_bytes;
+
+  for (int16_t y = fb->bounds.origin.y; y < fb->bounds.size.h; y++) {
+    const uint8_t row_gray = gray_for_row(y);
+
+    // as our dither pattern repeats itself after 8 pixels (8x8 dither matrix), we can store the
+    // whole pattern of this row in a single byte and set all pixels with a single call to memset
+    uint8_t row_gray_dither_pattern = dither_pattern_8(y, row_gray);
+    memset(row, row_gray_dither_pattern, fb->row_size_bytes);
+
+    // move row pointer to the start of the next row
+    row += fb->row_size_bytes;
+  }
+
+  // do not forget to release the frame buffer after usage. Other graphics_* functions are blocked
+  // while it is capture.
+  graphics_release_frame_buffer(ctx, fb);
+}
+
+/*
+ * Draws linear gradient once using direct frame buffer access + draws info about used method.
+ */
+static void update_proc_frame_buffer_1(Layer *layer, GContext *ctx) {
+  update_proc_frame_buffer(layer, ctx);
+  draw_method_feedback(ctx, "frame_buffer");
+}
+
+/*
+ * Draws linear gradient 20 times using direct frame buffer access + draws info about used method.
+ */
+static void update_proc_frame_buffer_20(Layer *layer, GContext *ctx) {
+  for(int i = 0; i < 20; i++) {
+    update_proc_frame_buffer(layer, ctx);
+  }
+  draw_method_feedback(ctx, "20 x frame_buffer");
+}
+
+/*
+ * Updates global helper variables and marks root layer dirty for the next animation step.
+ *
+ * This function calls app_timer_register() for itself to enter an infinite update loop.
+ */
+static void update_animation(void *data) {
+  // input_top/_bottom are angles and change with each update so s_gray_top/_bottom will update
+  // smoothly thanks to sin_lookup
+  // The magic numbers you see had been determined via trial and error
+  static int input_top = TRIG_MAX_ANGLE * 1 / 4;
+  static int input_bottom = TRIG_MAX_ANGLE * 3 / 4;
+  input_top += TRIG_MAX_ANGLE * 7 / 360;
+  input_bottom += TRIG_MAX_ANGLE * 3 / 360;
+
+  // gray values used for the actual drawing
+  s_gray_top = (127 + sin_lookup(input_top) * 127 / TRIG_MAX_RATIO);
+  s_gray_bottom = (127 + sin_lookup(input_bottom) * 127 / TRIG_MAX_RATIO);
+
+  // y-coordinate for draw_method_feedback to give additional visual feedback about the speed
+  s_time_feedback_y += 5;
+
+  // trigger the actual redraw
+  layer_mark_dirty(window_get_root_layer(s_window));
+
+  // enter the infinite update loop
+  app_timer_register(1000 / 30, update_animation, NULL);
+}
+
+static LayerUpdateProc update_procs[] = {
+    update_proc_frame_buffer_1,
+    update_proc_draw_pixel,
+    update_proc_frame_buffer_20,
+};
+
+static int s_current_update_proc = 0;
+
+static void toggle_update_proc_click_handler(ClickRecognizerRef recognizer, void *context) {
+  s_current_update_proc = (s_current_update_proc + 1) % ARRAY_LENGTH(update_procs);
+  layer_set_update_proc(window_get_root_layer(s_window), update_procs[s_current_update_proc]);
+}
+
+static void click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, toggle_update_proc_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, toggle_update_proc_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, toggle_update_proc_click_handler);
+}
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  layer_set_update_proc(window_layer, update_procs[0]);
+  s_window_size = layer_get_bounds(window_layer).size;
+
+  // start update loop
+  update_animation(NULL);
+}
+
+static void init(void) {
+  s_window = window_create();
+  window_set_click_config_provider(s_window, click_config_provider);
+  window_set_window_handlers(s_window, (WindowHandlers) {
+    .load = window_load,
+  });
+
+  const bool animated = true;
+  window_stack_push(s_window, animated);
+}
+
+static void deinit(void) {
+  window_destroy(s_window);
+}
+
+int main(void) {
+  init();
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", s_window);
+
+  app_event_loop();
+  deinit();
+  return 0;
+}
